77f4cba3763f87f944463243bf877535d0c3f565
Erika Conca <erikafconca@gmail.com>
2021-04-12 22:30:44 -0300

    Criacao do AutorRequest

---
+++ src/main/java/br/com/zupacademy/erikaconca/casadocodigo/autor/AutorRequest.java
@@ -0,0 +1,38 @@
+package br.com.zupacademy.erikaconca.casadocodigo.autor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class AutorRequest {
+
+    @NotBlank
+    private String nome;
+    @NotBlank @Email
+    private String email;
+    @NotBlank @Size(max = 400)
+    private String descricao;
+
+    public AutorRequest(String nome, String email, String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Autor toAutor(){
+        Autor autor = new Autor(nome, email, descricao);
+        return autor;
+    }
+}
