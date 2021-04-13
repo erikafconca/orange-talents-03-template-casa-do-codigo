c3bf4b40cb6a13b1a8ce621834e5f9b4048ef8b4
Erika Conca <erikafconca@gmail.com>
2021-04-13 14:49:50 -0300

    Verificar existencia de email

--- src/main/java/br/com/zupacademy/erikaconca/casadocodigo/autor/AutorRepository.java
+++ src/main/java/br/com/zupacademy/erikaconca/casadocodigo/autor/AutorRepository.java
@@ -3,4 +3,6 @@ package br.com.zupacademy.erikaconca.casadocodigo.autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    Boolean existsByEmail(String email);
 }
