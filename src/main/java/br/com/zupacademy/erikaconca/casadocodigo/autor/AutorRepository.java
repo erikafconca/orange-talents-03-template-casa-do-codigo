1eafffc4f6d3843f24d7cea925af56810a0d7374
Erika Conca <erikafconca@gmail.com>
2021-04-12 22:29:51 -0300

    Criacao de AutorRepository

---
+++ src/main/java/br/com/zupacademy/erikaconca/casadocodigo/autor/AutorRepository.java
@@ -0,0 +1,6 @@
+package br.com.zupacademy.erikaconca.casadocodigo.autor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
