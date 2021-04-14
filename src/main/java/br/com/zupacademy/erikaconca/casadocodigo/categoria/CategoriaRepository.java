21fe45728d146536f1706946b3a2ebacd15b83a9
Erika Conca <erikafconca@gmail.com>
2021-04-13 23:43:41 -0300

    Criação da interface CategoriaCRepository

---
+++ src/main/java/br/com/zupacademy/erikaconca/casadocodigo/categoria/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zupacademy.erikaconca.casadocodigo.categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository  extends JpaRepository<Categoria, Long> {
+
+}
